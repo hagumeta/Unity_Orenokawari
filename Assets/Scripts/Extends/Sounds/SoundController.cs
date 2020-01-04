1dcbf4bf7b6c355d03e82b62beca284f535ba6f6
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-01-05 05:09:08 +0900

    SoundControllerを追加
    音データ管理をしておくクラスを実装．

---
+++ Assets/Scripts/Extends/Sounds/SoundController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Extends.Sounds
+{
+    [ExecuteInEditMode]
+    public class SoundController : SingletonMonoBehaviourFast<SoundController>
+    {
+        protected override void Init()
+        {
+            Instance.LoadData();
+        }
+
+        protected Dictionary<string, List<Sound>> list;
+        public List<string> pathes;
+        public AudioMixer audioMixer;
+
+        protected void LoadData()
+        {
+            this.list = new Dictionary<string, List<Sound>>();
+            foreach (var path in this.pathes)
+            {
+                this.list.Add(path, new List<Sound>(Resources.LoadAll<Sound>(path)));
+            }
+        }
+
+        public AudioMixerGroup GetAudioMixerGroup(string key)
+        {
+            AudioMixerGroup[] groups = this.audioMixer.FindMatchingGroups(key);
+            if (groups.Length < 1)
+            {
+                return null;
+            }
+            else
+            {
+                return groups[0];
+            }
+        }
+
+        public List<Sound> GetSoundsList(string key)
+        {
+            if (this.list.ContainsKey(key))
+            {
+                return this.list[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
